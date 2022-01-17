b99e0a43a80632472ee73c8236421c9246415cb8
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2022-01-17 09:24:19 +0100

    Hotové

--- ukol1/ukol1/Form1.cs
+++ ukol1/ukol1/Form1.cs
@@ -16,5 +16,58 @@ namespace ukol1
         {
             InitializeComponent();
         }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "0";
+            }
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "0";
+            }
+            int jedna = int.Parse(textBox1.Text); //N 
+            int dva = int.Parse(textBox2.Text); //A
+            int A = 1; // pomocne
+            int N = 1; // pomocne
+            
+            if (checkBox1.Checked == true )
+            {
+                // A na Ntou
+                for (int pomocna = jedna; pomocna != 0; pomocna--)
+                {
+                    A = A * dva;
+                }
+                vysledek.Text = "A na 'Ntou' " + A;
+
+            }
+            if (checkBox2.Checked == true)
+            {
+                // faktorial N
+                if (jedna == 0)
+                {
+                    vysledek.Text = "\nFaktorial N! " + "1";
+                }
+                else
+                {
+                    for (int pomocna = jedna; pomocna != 1; pomocna--)
+                    {
+                        N = N * pomocna;
+                    }
+                    vysledek.Text = "\nFaktorial N! " + N;
+                }
+
+            }
+
+            if (checkBox1.Checked == true && checkBox2.Checked == true)
+            {
+                vysledek.Text = "A na 'Ntou' " + A +"\nFaktorial N! " + N;
+                
+            }
+
+
+
+        }
     }
 }
