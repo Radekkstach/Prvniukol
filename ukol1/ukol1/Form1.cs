2524e0bc3f99165f4248133e89a2d32cf65259f9
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2022-01-20 21:36:43 +0100

    pridani try,catch hotovo

--- ukol1/ukol1/Form1.cs
+++ ukol1/ukol1/Form1.cs
@@ -27,46 +27,56 @@ namespace ukol1
             {
                 textBox2.Text = "0";
             }
-            int jedna = int.Parse(textBox1.Text); //N 
-            int dva = int.Parse(textBox2.Text); //A
-            int A = 1; // pomocne
-            int N = 1; // pomocne
-            
-            if (checkBox1.Checked == true )
+            try
             {
-                // A na Ntou
-                for (int pomocna = jedna; pomocna != 0; pomocna--)
-                {
-                    A = A * dva;
-                }
-                vysledek.Text = "A na 'Ntou' " + A;
+                int jedna = int.Parse(textBox1.Text); //N 
+                int dva = int.Parse(textBox2.Text); //A
+                int A = 1; // pomocne
+                int N = 1; // pomocne
 
-            }
-            if (checkBox2.Checked == true)
-            {
-                // faktorial N
-                if (jedna == 0)
+                if (checkBox1.Checked == true)
                 {
-                    vysledek.Text = "\nFaktorial N! " + "1";
-                }
-                else
-                {
-                    for (int pomocna = jedna; pomocna != 1; pomocna--)
+                    // A na Ntou
+                    for (int pomocna = jedna; pomocna != 0; pomocna--)
                     {
-                        N = N * pomocna;
+                        A = A * dva;
                     }
-                    vysledek.Text = "\nFaktorial N! " + N;
+                    vysledek.Text = "A na 'Ntou' " + A;
+
+                }
+                if (checkBox2.Checked == true)
+                {
+                    // faktorial N
+                    if (jedna == 0)
+                    {
+                        vysledek.Text = "\nFaktorial N! " + "1";
+                    }
+                    else
+                    {
+                        for (int pomocna = jedna; pomocna != 1; pomocna--)
+                        {
+                            N = N * pomocna;
+                        }
+                        vysledek.Text = "\nFaktorial N! " + N;
+                    }
+
+                }
+
+                if (checkBox1.Checked == true && checkBox2.Checked == true)
+                {
+                    vysledek.Text = "A na 'Ntou' " + A + "\nFaktorial N! " + N;
+
                 }
 
             }
-
-            if (checkBox1.Checked == true && checkBox2.Checked == true)
+            catch (FormatException x)
             {
-                vysledek.Text = "A na 'Ntou' " + A +"\nFaktorial N! " + N;
-                
+                MessageBox.Show(x.Message);
             }
-
-
+            catch(OverflowException y)
+            {
+                MessageBox.Show(y.Message);
+            }
 
         }
     }
